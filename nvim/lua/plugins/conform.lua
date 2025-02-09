01293c1532dca1f5ecbdf66b491f9e770544f0f5
Jan Tumpa <72min@proton.me>
2025-02-09 18:57:55 +0100

    Add files via upload

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,36 @@
+return {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+        {
+            '<leader>f',
+            function()
+                require('conform').format({ async = true, lsp_fallback = true })
+                vim.api.nvim_create_autocmd('BufWritePre', {
+                    pattern = '*',
+                    callback = function(args)
+                        require('conform').format({ bufnr = args.buf })
+                    end,
+                })
+            end,
+            mode = '',
+            desc = '[F]ormat buffer',
+        },
+    },
+    opts = {
+        notify_on_error = true,
+        format_on_save = function()
+            return {
+                timeout_ms = 500,
+                lsp_fallback = true,
+            }
+        end,
+        formatters_by_ft = {
+            lua = { 'stylua' },
+            python = { 'ruff', 'black', stop_after_first = true },
+            javascript = { 'biome', 'prettierd', stop_after_first = true },
+            html = { 'prettierd' },
+        },
+    },
+}
