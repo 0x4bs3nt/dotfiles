c7b0fc4a4c50e57b0df06d6d76a52dbb6ec74dc5
0x4bs3nt <hi@4bs3nt.com>
2026-03-15 02:02:18 +0100

    eza

--- fish/config.fish
+++ fish/config.fish
@@ -34,6 +34,7 @@ if status is-interactive
 end
 set -gx EDITOR nvim
 alias lg="lazygit"
+alias ll="eza --long --all --group --group-directories-first --icons=auto --header --colour-scale=size --time-style=relative"
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
